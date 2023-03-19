d1e40f31dec87d4aef97e3ad8835fb51d2d1f49e
Elena Surilova <elen21surilova@gmail.com>
2023-03-19 22:33:13 +0200

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: "/tech-task-genesis-frontend-school/",
+  base: "/tech-task-genesis-frontend-school/",
   plugins: [vue()],
   resolve: {
     alias: {
